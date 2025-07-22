9a40983cf1df4b5f7947e848fb96d43f24b15be8
shaswatkr19 <shaswatsinha000@gmail.com>
2025-07-23 00:42:12 +0530

    update some changes

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "jhol" : "https://youtu.be/-2RAq5o5pwc?si=q11CtVhjVDjIPsFj",
+    "saiyaara" : "https://youtu.be/BSJa1UytM8w?si=OS5tY_23v4t2a68W",
+    "ishq" : "https://youtu.be/hHuG7FIKgtc?feature=shared",
+    "sahiba" : "https://youtu.be/n2dVFdqMYGA?feature=shared",
+    "daaku" : "https://youtu.be/WOdnRhWeHoY?feature=shared"
+}
